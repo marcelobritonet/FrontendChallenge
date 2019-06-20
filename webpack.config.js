1e40ea27bfea1942e5771b814b5cf66e2ad5861a
marcelobritonet <m#Cro%t8jZS1ps2Y^@CbIp!aCXp3zsY6o61GljgI*lihXEiDi21Iu3WdsW^iDxCLUH@Y!$6#>
2019-06-20 16:22:04 -0300

    chore: setting babel and webserver

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ module.exports = {
     entry: './app.module.js',
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // which file needs to be read
+                exclude: /node_modules/, // which folder needs not to be read
+                loader: ['babel-loader'] // which transplier/compiler/plugin to compile files
+            }
+        ]
+    },
+    devServer: {
+        port: 3000, // configuring port for localserver
+        contentBase: './' // configuring from where content needs to be served
     }
 };
